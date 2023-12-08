e785706ecc434bd54445f5d6e9990af39ce4f537
UnknownDev2018 <98718931+UnknownDev2018@users.noreply.github.com>
2023-12-08 09:23:44 -0800

    Update NvidiaProfile.bat

--- v2/NvidiaProfile.bat
+++ v2/NvidiaProfile.bat
@@ -2,23 +2,22 @@
 echo.
 echo Nvidia Boost FPS v3
 
-
-:: Crear directorio si no existe
+:: Create directory if it doesn't exist
 md C:\Tobby
 
-:: Descargar nvidiaProfileInspector.zip
-curl -g -k -L -# -o "%temp%\nvidiaProfileInspector.zip" "https://github.com/Orbmu2k/nvidiaProfileInspector/releases/latest/download/nvidiaProfileInspector.zip" >nul 2>&1
+:: Download nvidiaProfileInspector.zip
+powershell -command "& {Invoke-WebRequest -Uri 'https://github.com/Orbmu2k/nvidiaProfileInspector/releases/latest/download/nvidiaProfileInspector.zip' -OutFile '%temp%\nvidiaProfileInspector.zip'}" >nul 2>&1
 
-:: Descargar Tobby_bestprofile.nip
-curl -g -k -L -# -o "C:\Tobby\Tobby_bestprofile.nip" "https://raw.githubusercontent.com/UnknownDev2018/TobbyTweaks/main/v2/Tobby_bestprofile.nip"
+:: Download Tobby_bestprofile.nip
+powershell -command "& {Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/UnknownDev2018/TobbyTweaks/main/v2/Tobby_bestprofile.nip' -OutFile 'C:\Tobby\Tobby_bestprofile.nip'}"
 
 cls
 chcp 437 >nul 2>&1
 
-:: Extraer nvidiaProfileInspector.zip
-powershell -NoProfile Expand-Archive '%temp%\nvidiaProfileInspector.zip' -DestinationPath 'C:\Tobby\' >nul 2>&1
+:: Extract nvidiaProfileInspector.zip
+powershell -NoProfile Expand-Archive -Path '%temp%\nvidiaProfileInspector.zip' -DestinationPath 'C:\Tobby\' >nul 2>&1
 
-:: Ejecutar nvidiaProfileInspector sin mostrar prompts
+:: Run nvidiaProfileInspector without prompts
 start "" /wait "C:\Tobby\nvidiaProfileInspector.exe" "C:\Tobby\Tobby_bestprofile.nip" /silent
 
 exit
